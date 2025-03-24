a0d09bf6158fda99b0a238e0790595ea48623111
Hans Jerry Illikainen <hji@dyntopia.com>
2025-03-24 04:17:09 +0000

    symmetric: remove helpers

--- src/symmetric/symmetric.go
+++ src/symmetric/symmetric.go
@@ -1,40 +1,6 @@
 package symmetric
 
-import (
-	"github.com/illikainen/go-cryptor/src/cryptor"
-	"github.com/illikainen/go-cryptor/src/symmetric/aesgcm"
-	"github.com/illikainen/go-cryptor/src/symmetric/xchacha20poly1305"
-
-	log "github.com/sirupsen/logrus"
-)
-
 type Keys struct {
 	AESGCM            string
 	XChaCha20Poly1305 string
-}
-
-func GenerateKey(kind int) (cryptor.SymmetricKey, error) {
-	log.Tracef("generate symmetric key for kind=%d", kind)
-
-	switch kind {
-	case cryptor.AESGCMSymmetric:
-		return aesgcm.GenerateKey()
-	case cryptor.XCHACHA20POLY1305Symmetric:
-		return xchacha20poly1305.GenerateKey()
-	}
-
-	return nil, cryptor.ErrInvalidKeyType
-}
-
-func ReadKey(kind int, data []byte) (cryptor.SymmetricKey, error) {
-	log.Tracef("read symmetric key for kind=%d", kind)
-
-	switch kind {
-	case cryptor.AESGCMSymmetric:
-		return aesgcm.ReadKey(data)
-	case cryptor.XCHACHA20POLY1305Symmetric:
-		return xchacha20poly1305.ReadKey(data)
-	}
-
-	return nil, cryptor.ErrInvalidKeyType
 }
